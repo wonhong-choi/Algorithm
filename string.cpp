68dda4e82b5341b58a654b9196aab92b36cedda6
wonhong <cwh0099@naver.com>
2022-12-28 21:51:08 +0900

    Update string algorithm 2/3

--- string.cpp
+++ string.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stack>
 #include <string>
 #include <vector>
 #include <string.h>
@@ -266,6 +267,171 @@ int leftMostNonOptimized(const string& str){
 }
 
 
+// Reverse words in a string
+// using auxiliary vector with two pointer
+// T/C : O(n)
+// S/C : O(n)
+string reverseWords(string s){
+    vector<string> tokens;
+    int left=0, right=0;
+    while(left<s.size()){
+        right=left;
+        while(right<s.size() && s[right]!=' '){
+            ++right;
+        }
+        tokens.push_back(s.substr(left,right-left));
+        left=right+1;
+    }
+
+    left=0, right=tokens.size()-1;
+    while(left<right){
+        string tmp=tokens[left];
+        tokens[left]=tokens[right];
+        tokens[right]=tmp;
+        left++;
+        right--;
+    }
+
+    string reversed="";
+    for(const string& token : tokens){
+        reversed+=token + " ";
+    }
+    return reversed;
+}
+// Reverse words in a string
+// using auxiliary stack
+// T/C : O(n)
+// S/C : O(n)
+string reverseWordsStack(string s){
+    stack<string> stack;
+    int left=0, right=0;
+    while(left<s.size()){
+        right=left;
+        while(right<s.size() && s[right]!=' '){
+            ++right;
+        }
+        stack.push(s.substr(left, right-left));
+        left=right+1;
+    }
+
+    s="";
+    while(!stack.empty()){
+        s+=stack.top() + " ";
+        stack.pop();
+    }
+    return s;
+}
+// double reversing with two pointer
+// T/C : O(n)
+// S/C : O(1)
+string reverseWordsOptimized(string s){
+    reverse(s.begin(), s.end());
+
+    int left=0, right=0;
+    while(left<s.size()){
+        right=left;
+        while(right<s.size() && s[right]!=' '){
+            right++;
+        }
+
+        reverse(s.begin()+left, s.begin()+right);
+        left=right+1;
+    }
+    return s;
+}
+
+
+// Pattern Searching
+// naive method
+// T/C : O((n-m+1)*m)
+// S/C : O(1)
+vector<int> patternSearching(const string& txt, const string& pattern){
+    vector<int> matching;
+    for(int i=0; i<=txt.size()-pattern.size();++i){
+        bool found=true;
+        for(int j=0; j<pattern.size(); ++j){
+            if(txt[i+j]!=pattern[j]){
+                found=false;
+                break;
+            }
+        }
+        if(found){
+            matching.push_back(i);
+        }
+    }
+    return matching;
+}
+// Improved naive method when in case which the characters of the pattern are distinct
+// T/C : O(n)
+// S/C : O(1)
+vector<int> patternSearchingImproved(const string& txt, const string& pattern){
+    vector<int> matching;
+    int i=0, j=0;
+    while(i<=txt.size()-pattern.size()){
+        j=0;
+        while(j<pattern.size() && i+j<txt.size() && txt[i+j]==pattern[j]){
+            ++j;
+        }
+        if(j==pattern.size()){
+            matching.push_back(i);
+        }
+        if(j==0)
+            i++;
+        else
+            i+=j;
+    }
+    return matching;
+}
+// Rabin Karp Algorithm
+// T/C : O((n-m+1) * m), but BETTER in general.
+// S/C : O(1)
+// IDEA : only run naive matching algorithm, only when the hash value of window is same as it of pattern.
+//        also, use rolling hash like (weighted) sum of window for traversal to minimize T/C
+//        ex. H(s[i+1:i+1+m]) = H(s[i:i+m]) - H(s[i]) + H(s[i+1+m])
+//        likely, rolling hash function takes just O(1) to calculate H(s[i+1:i+1+m]) from H(s[i:i+m])
+
+// when, using hash function as just sum of window
+vector<int> patternSearchingImproved(const string& txt, const string& pattern){
+    int patternHash=0;
+    for(char c : pattern){
+        patternHash+=c;
+    }
+
+    int windowHash=0;
+    for(int i=0; i<pattern.size();++i){
+        windowHash+=txt[i];
+    }
+
+    vector<int> matching;
+    for(int i=pattern.size(); i<=txt.size()-pattern.size(); ++i){
+        if(windowHash==patternHash){        // naive matching, if true
+            int j=0;
+            while(j<pattern.size() && pattern[j]==txt[i-pattern.size()+j])
+                ++j;
+            
+            if(j==pattern.size()){
+                matching.push_back(i-pattern.size());
+            }
+        }
+
+        windowHash-=txt[i-pattern.size()];
+        windowHash+=txt[i];
+    }
+
+    if(windowHash==patternHash){        // naive matching, if true
+        int j=0;
+        while(j<pattern.size() && pattern[j]==txt[txt.size()-pattern.size()+j])
+            ++j;
+            
+        if(j==pattern.size()){
+            matching.push_back(txt.size()-pattern.size());
+        }
+    }
+
+    return matching;
+}
+
+
 int main(){
     //printFrequencies("geeksforgeeks");
     //stringInCPP();
@@ -273,6 +439,11 @@ int main(){
     //cout << isSubsequenceRecursive("GEEKSFORGEEKS", "GFGES", 12, 4) << endl;
     //cout << isAnagramOptimized("abaac", "aaabc") << endl;
     //cout << leftMostMostOptimized("cabbad") << endl;
-    cout << leftMostNonOptimized("abbcbda") << endl;
+    // cout << leftMostNonOptimized("abbcbda") << endl;
+    //cout << reverseWordsStack("welcome to gfg") << endl;
+    auto vec = patternSearchingImproved("abcaada", "abd");
+    for(auto v : vec){
+        cout << v << endl;
+    }
     return 0;
 }
