302a567d09ef3b08f1532601774a1f50c289e505
wonhongchoi <cwh0099@naver.com>
2023-02-01 17:05:26 +0900

    initalize graph

---
+++ Graph.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/*
+    GRAPH
+
+    1) Graph Representation
+        1-1) Adjacency Matrix
+            Operations:
+                space : O(v * v)
+                Check if u and v are adjacent : O(1)
+                Find all vertices adjacent to u : O(v)
+                Find degree of U : O(v)
+                Add/Remove an edge : O(1)
+                Add/Remove an vertix : O(v*v)
+        
+        1-2) Adjacency List
+            Operations:
+                space : O(v * v)
+                Check if u and v are adjacent : O(1)
+                Find all vertices adjacent to u : O(v)
+                Find degree of U : O(v)
+                Add/Remove an edge : O(1)
+                Add/Remove an vertix : O(v*v)
+
+
+
+*/
+
+
+int main(){
+
+}
